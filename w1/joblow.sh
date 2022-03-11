f3b41d74e4449c4eee12a5e3bb3d57d7ee9e6d3c
rachidboukir <rachid.boukir@autonoma.cat>
2022-03-11 20:53:18 +0100

    Update joblow.sh

--- w1/joblow.sh
+++ w1/joblow.sh
@@ -5,9 +5,4 @@
 #SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
 #SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e %x_%u_%j.err # File to which STDERR will be written
-
-# Activate Anaconda work environment for OpenDrift
-source /home/group01/.bashrc
-source activate my_env 
-
-python main.py /home/group01/MCV_2022_M5_Project/w1/configs/test.json
+python3.8 main.py /home/group01/MCV_2022_M5_Project/w1/configs/test.json
