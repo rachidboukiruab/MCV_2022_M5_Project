3942b33af0b693d6900de91fdc8df6a0fce7cc19
rachidboukir <rachid.boukir@autonoma.cat>
2022-03-12 15:55:14 +0100

    dimensionality fix in Team3Model

--- w1/models.py
+++ w1/models.py
@@ -63,8 +63,8 @@ class Team3Model(nn.Module):
         self.conv2 = nn. Conv2d(16, 32, 3)
         self.conv3 = nn. Conv2d(32, 64, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.Avgpool = nn.AvgPool2d(8)
-        self.linear = nn.Linear(384, self.nclasses)       
+        self.Avgpool = nn.AvgPool2d(2)
+        self.linear = nn.Linear(64, self.nclasses)       
     
     def forward(self, x):
         x = self.pool(nn.functional.relu(self.conv1(x)))  
