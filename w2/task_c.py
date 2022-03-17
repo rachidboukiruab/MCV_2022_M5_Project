3e8e605087bb8c376817cc78438b14394e1d6f5d
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-17 11:01:40 +0100

    adding Mask-RCNN

--- w2/task_c.py
+++ w2/task_c.py
@@ -67,6 +67,7 @@ if __name__ == '__main__':
                 img_path = os.path.join(root, file)
                 out_path2 = os.path.join(out_path, file)
 
+                print(f"root {}, file {file}, out_path {out_path}")
                 print(f"reading from {img_path} saved into {out_path2}")
 
                 # img = inference(img_path)
