5e3c7378b90e712b7c72be38ec30037c888ef8c3
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-03-20 00:19:20 +0100

    prueba1

--- w2/dataset_dict.py
+++ w2/dataset_dict.py
@@ -36,6 +36,7 @@ class DatasetSplit(TypedDict):
 
 
 def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
+    dataset_dicts = []
     with open('./configs/dataset_split.json') as f_splits:
         sequences = json.load(f_splits)[part]
 
@@ -62,6 +63,7 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
                        "height": int, "width": int, "rle": str}
             )
         for img_path in seq.glob("*.png"):
+
             img_name = img_path.parts[-1]
             frame = int(img_path.parts[-1].split('.')[0])
             frame_gt = (gt[gt["frame"] == frame])
@@ -69,8 +71,13 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
             if len(frame_gt) == 0:
                 continue
 
-            ann = []
+            objs = []
             for _, obj_id, class_id, height, width, rle in frame_gt.itertuples(index=False):
+                record = {}
+                record["file_name"] = str(img_path)
+                record["height"] = frame_gt.iloc[0]["height"]
+                record["width"] = frame_gt.iloc[0]["width"]
+                record["image_id"] = int(f"{sequence}{frame:05}")
 
                 # reads rle and decodes it with cocotools
                 rle = bytearray(rle, "utf8")
@@ -78,22 +85,15 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
                 rleobj = frPyObjects([rle], height, width)[0]
                 bbox = toBbox(rleobj)
 
-                ann.append({
+                obj = {
                     "bbox": bbox.flatten(),
-                    "bbox_mode": BoxMode.XYWH_ABS,
-                    "category_id": class_id,
+                    "bbox_mode": BoxMode.XYXY_ABS,
                     "segmentation": rleobj,
-                    "keypoints": [],
+                    "category_id": class_id,
                     "iscrowd": 0
-                })
+                }
+                objs.append(obj)
+                record["annotations"] = objs
+                dataset_dicts.append(record)
 
-            anns.append({
-                "file_name": str(img_path),
-                "height": frame_gt.iloc[0]["height"],
-                "width": frame_gt.iloc[0]["width"],
-                "image_id": int(f"{sequence}{frame:05}"),
-                "sem_seg": str(path / "instances" / sequence / img_name),
-                "annotations": ann
-            })
-
-    return anns
+    return dataset_dicts
