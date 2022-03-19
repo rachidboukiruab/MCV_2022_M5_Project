ea4dc0f6616e6a75d94c845a16e5c2db9b920470
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-03-19 22:11:55 +0100

    debug taskd

--- w2/dataset_dict.py
+++ w2/dataset_dict.py
@@ -30,7 +30,7 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
 
     for seq in root_img_dir.glob("*"):
         sequence = seq.parts[-1]
-
+        dataset_dicts = []
         # Ensure the sequence belongs to the selected partition
         if sequence not in sequences:
             continue
@@ -52,6 +52,12 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
             if len(frame_gt) == 0:
                 continue
 
+            record = {}
+            record["file_name"] = str(img_path)
+            record["height"] = frame_gt.iloc[0]["height"]
+            record["width"] = frame_gt.iloc[0]["width"]
+            record["image_id"] = int(f"{sequence}{frame:05}")
+
             ann = []
             for _, obj_id, class_id, height, width, rle in frame_gt.itertuples(index=False):
                 rle = bytearray(rle, "utf8")
@@ -63,18 +69,19 @@ def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
                     "bbox_mode": BoxMode.XYWH_ABS,
                     "category_id": class_id,
                     "segmentation": rleobj,
-                    "keypoints": [],
+                    #"keypoints": [],
                     "iscrowd": 0
                 })
-
-            anns.append({
-                "file_name": str(img_path),
-                "height": frame_gt.iloc[0]["height"],
-                "width": frame_gt.iloc[0]["width"],
-                "image_id": int(f"{sequence}{frame:05}"),
-                "sem_seg": str(path / "instances" / sequence / img_name),
-                "annotations": ann
-            })
-    return anns
+            record["annotations"] = ann
+            dataset_dicts.append(record)
+            # anns.append({
+            #     "file_name": str(img_path),
+            #     "height": frame_gt.iloc[0]["height"],
+            #     "width": frame_gt.iloc[0]["width"],
+            #     "image_id": int(f"{sequence}{frame:05}"),
+            #     "sem_seg": str(path / "instances" / sequence / img_name),
+            #     "annotations": ann
+            # })
+    return dataset_dicts
 
 
