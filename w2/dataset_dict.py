1d7d01b1cfa4bcbefdd8c7f7f30e3873f53595fe
ptorras <pautorras1@hotmail.com>
2022-03-19 21:24:55 +0100

    merge

--- w2/dataset_dict.py
+++ w2/dataset_dict.py
@@ -1,13 +1,11 @@
 import pandas as pd
 from pathlib import Path
 from typing import List, Dict
-from pycocotools.mask import decode, toBbox, frPyObjects
+from pycocotools.mask import toBbox, frPyObjects
 from detectron2.structures import BoxMode
-from detectron2.data import MetadataCatalog, DatasetCatalog
-#from typing import TypedDict, List
+
 from typing_extensions import TypedDict
 import json
-import os
 
 
 class DatasetSplit(TypedDict):
@@ -17,48 +15,41 @@ class DatasetSplit(TypedDict):
     """
     val: List
     training: List
-    #test_set: List
-    
+    # test_set: List
 
-def get_KITTI_dataset(path: str, part: str) -> List[Dict]:
-    
-    with open('/home/group01/MCV_2022_M5_Project/w2/configs/dataset_split.json') as f:
-        exp: DatasetSplit = json.load(f)
-  
+
+def get_KITTI_dataset(path: Path, part: str) -> List[Dict]:
+    with open('./configs/dataset_split.json') as f_splits:
+        sequences = json.load(f_splits)[part]
+
+    if part == "val":
+        part = "training"
+
+    root_img_dir = path / part / "image_02"
     anns = []
 
-    for i in exp[part]:
-      root_img_dir = f'{path}/training/image_02/{i}'
-      
-      
-      for seq in Path(root_img_dir).glob("*"):
-      #for (root, dirs, files) in os.walk(root_img_dir, topdown=True):
-          #sequence = Path(root).parts[-1]
-          sequence = seq.parts[-2]
-          #with open(f'KITTI-MOTS/instances_txt/{sequence}.txt') as f_ann:
-          with open(f'{path}/instances_txt/{sequence}.txt') as f_ann:
-              
-              gt = pd.read_table(
-                  f_ann,
-                  sep=" ",
-                  header=0,
-                  names=["frame", "obj_id", "class_id", "height", "width", "rle"],
-                  dtype={"frame": int, "obj_id": int, "class_id": int,
-                      "height": int, "width": int, "rle": str}
-              )
-          '''for img in files:
-              img_path = root / img
-              frame = int(Path(img).parts[-1].split('.')[0])
-              frame_gt = (gt[gt["frame"] == frame])'''
-              
-          for img_path in seq.glob("*.png"):
-            print(img_path)
+    for seq in root_img_dir.glob("*"):
+        sequence = seq.parts[-1]
+
+        # Ensure the sequence belongs to the selected partition
+        if sequence not in sequences:
+            continue
+
+        with open(path / "instances_txt" / (sequence + ".txt")) as f_ann:
+            gt = pd.read_table(
+                f_ann,
+                sep=" ",
+                header=0,
+                names=["frame", "obj_id", "class_id", "height", "width", "rle"],
+                dtype={"frame": int, "obj_id": int, "class_id": int,
+                       "height": int, "width": int, "rle": str}
+            )
+        for img_path in seq.glob("*.png"):
             img_name = img_path.parts[-1]
             frame = int(img_path.parts[-1].split('.')[0])
             frame_gt = (gt[gt["frame"] == frame])
-            print(frame)
-              
-            if not len(frame_gt):
+
+            if len(frame_gt) == 0:
                 continue
 
             ann = []
@@ -76,12 +67,13 @@ def get_KITTI_dataset(path: str, part: str) -> List[Dict]:
                 })
 
             anns.append({
-              "file_name": str(img_path),
-              "height": frame_gt["height"].iloc[0],
-              "width": frame_gt["width"].iloc[0],
-              "image_id": int(f"{sequence}{frame:05}"),
-              "sem_seg": str(path / "instances" / sequence / img_name),
-              "annotations": ann
-          })
-
+                "file_name": str(img_path),
+                "height": frame_gt.iloc[0]["height"],
+                "width": frame_gt.iloc[0]["width"],
+                "image_id": int(f"{sequence}{frame:05}"),
+                "sem_seg": str(path / "instances" / sequence / img_name),
+                "annotations": ann
+            })
     return anns
+
+
--- w2/task_d.py
+++ w2/task_d.py
@@ -11,43 +11,36 @@ from dataset_dict import get_KITTI_dataset
 
 setup_logger()
 
-
-dataset_dir = '/home/mcv/datasets/KITTI-MOTS/'
-results_dir = './results/task_d/'
+# FIXME: Add the correct path here
+dataset_dir = Path("/home/pau/Documents/datasets/kitti-mots")
+results_dir = Path('./results/task_d/')
 os.makedirs(results_dir, exist_ok=True)
-
-
 
 if __name__ == '__main__':
 
-    model_list = ['COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml', 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml']
-    
+    model_list = ['COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml',
+                  'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml']
 
-    for d in ['val']:
-      DatasetCatalog.register("KITTI-MOTS_" + d, lambda d=d: get_KITTI_dataset(dataset_dir, d))
-      MetadataCatalog.get("KITTI-MOTS_" + d).set(thing_classes=["Car", "Pedestrian"])
+    for d in ['training', 'val']:
+        DatasetCatalog.register("KITTI-MOTS_" + d, lambda d=d: get_KITTI_dataset(dataset_dir, d))
+        # MetadataCatalog.get("KITTI-MOTS_" + d).set(thing_classes=["Car", "Pedestrian"])
     metadata = MetadataCatalog.get("KITTI-MOTS_val")
-    
-    
-    for model_yalm in model_list:
-        
-        print('Creating dataset')
 
+    for model_yaml in model_list:
+        print('Creating dataset')
         dataset_dicts = get_KITTI_dataset(dataset_dir, 'val')
 
         cfg = get_cfg()
 
-        cfg.merge_from_file(model_zoo.get_config_file(model_yalm))
         cfg.DATASETS.VAL = "KITTI-MOTS_val"
+        cfg.merge_from_file(model_zoo.get_config_file(model_yaml))
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_yalm)
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_yaml)
         predictor = DefaultPredictor(cfg)
 
         print('Evaluating model')
-        
+
         """ EVALUATION """
-        evaluator = COCOEvaluator("KITTI-MOTS_val", output_dir=results_dir)
+        evaluator = COCOEvaluator("KITTI-MOTS_val", output_dir=str(results_dir))
         val_loader = build_detection_test_loader(cfg, "KITTI-MOTS_val")
         print(inference_on_dataset(predictor.model, val_loader, evaluator))
-
-
