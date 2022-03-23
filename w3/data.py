ba2225aabb52d35002773a8e11aed9b016ccf9ef
ptorras <pautorras1@hotmail.com>
2022-03-23 19:51:03 +0100

    task a written and working

--- w3/data.py
+++ w3/data.py
@@ -1,1 +1,35 @@
 from pathlib import Path
+from typing import List, Dict
+
+from PIL import Image
+
+
+def dataset_from_image_dir(path: Path) -> List[Dict]:
+    """
+    Create a list of dict from an image path in order to perform inference.
+
+    Parameters
+    ----------
+    path: Path
+        Path to the folder where images are stored.
+
+    Returns
+    -------
+    List[Dict]
+        A list of dictionaries in Detectron Format (ready to register).
+    """
+    output = []
+    input_files = [x for x in path.glob("*") if x.is_file()]
+    input_files.sort(key=str)
+
+    for ii, img_path in input_files:
+        img = Image.open(img_path)
+
+        output.append({
+            "file_name": str(img_path),
+            "height": img.height,
+            "width": img.width,
+            "image_id": ii,
+        })
+
+    return output
