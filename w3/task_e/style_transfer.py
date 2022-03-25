af26cf3f09b6a9a10a4344007558357668d60c53
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-25 20:11:08 +0100

    content size bug

--- w3/task_e/style_transfer.py
+++ w3/task_e/style_transfer.py
@@ -3,7 +3,9 @@ from os import listdir
 from os.path import isfile, join
 
 import torchvision.models as models
+from PIL import Image
 from torchvision.utils import save_image
+
 from config import RESULT_PATH, STYLE_PATH, CONTENT_PATH, NUM_STEPS, CONTENT_WEIGHT, STYLE_WEIGHT
 from utils import image_loader, make_dirs, device, run_style_transfer, cnn_normalization_mean, cnn_normalization_std
 
@@ -27,10 +29,12 @@ for ii, (style, content) in enumerate(zip(style_images, content_images)):
     # print(style)
     # print(content)
 
-    content_size = content.shape
+    content_size = Image.open(content).shape
 
     style_img = image_loader(style, max(content_size))
     content_img = image_loader(content, max(content_size))
+
+
 
     assert style_img.size() == content_img.size(), "style & content imgs should be same size"
 
