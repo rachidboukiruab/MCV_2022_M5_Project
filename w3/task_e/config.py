2707f6dea7512302b06d28cd61ce27b3dd648a55
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-25 09:17:43 +0100

    all paths to config.py

--- w3/task_e/config.py
+++ w3/task_e/config.py
@@ -1,3 +1,4 @@
 RESULT_PATH = '/home/group01/MCV_2022_M5_Project/w3/te_data/results'
 STYLE_PATH = "/home/group01/MCV_2022_M5_Project/w3/te_data/style"
 CONTENT_PATH = "/home/group01/MCV_2022_M5_Project/w3/te_data/content"
+IMSIZE = (820,820)
--- w3/task_e/utils.py
+++ w3/task_e/utils.py
@@ -8,6 +8,8 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from PIL import Image
 
+from config import IMSIZE
+
 content_layers_default = ['conv_4']
 style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -16,7 +18,7 @@ cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
 loader = transforms.Compose([
-    transforms.Resize((imsize, imsize)),  # scale imported image
+    transforms.Resize(IMSIZE),  # scale imported image
     transforms.ToTensor()])  # transform it into a torch tensor
 
 
