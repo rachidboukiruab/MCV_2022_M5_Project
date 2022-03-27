542498102e5387a329fd785a47f79f93ed6ddd0d
imcosmicwol <marcos.melgar.segovia@gmail.com>
2022-03-27 17:25:01 +0200

    new style transfer hymper

--- w3/task_e/config.py
+++ w3/task_e/config.py
@@ -3,5 +3,5 @@ STYLE_PATH = "/home/group01/MCV_2022_M5_Project/w3/te_data/style"
 CONTENT_PATH = "/home/group01/MCV_2022_M5_Project/w3/te_data/content"
 
 NUM_STEPS = 900
-CONTENT_WEIGHT = 0.5
+CONTENT_WEIGHT = 10
 STYLE_WEIGHT = 10000
