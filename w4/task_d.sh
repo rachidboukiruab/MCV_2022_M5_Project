e9646d08e74517f0ba1c05736c7ec4df57627c9a
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-02 18:35:25 +0200

    added sh for task e

---
+++ w4/task_d.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -n 4 # Number of cores
+#SBATCH --mem 4096 # 4GB solicitados.
+#SBATCH -p mhigh,mhigh # or mlow Partition to submit to master low prioriy queue
+#SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
+#SBATCH -o ./slurm_logs.%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e ./slurm_logs.%x_%u_%j.err # File to which STDERR will be written
+python3.8 task_d.py
