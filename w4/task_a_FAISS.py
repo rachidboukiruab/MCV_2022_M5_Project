3a83a1a3d9797d01007bcbc1ed1c023f1d05b0ff
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-02 20:51:25 +0200

    cpu

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -27,29 +27,21 @@ def build_index(model, train_dataset, d=32):
 
     res = faiss.StandardGpuResources()  # defines resource, use a single GPU
     index = faiss.IndexFlatL2(d)  # build the index
-    gpu_index = faiss.index_cpu_to_gpu(res, 0, index)  # make it into a gpu index
-    print(gpu_index.is_trained)
 
     id = 0
     for data, label in train_dataset:
         xb = model(data)
         img_dict = {id: (label, xb)}
-        gpu_index.add(img_dict)  # add vectors to the index
+        index.add(img_dict)  # add vectors to the index
 
         # SANITY TODO: remove this after debugging
-        D, I = gpu_index.search(xb[:5], 4)  # sanity check
+        D, I = index.search(xb[:5], 4)  # sanity check
         print(
             "As a sanity check, we can first search a few database vectors, to make sure the nearest neighbor is indeed the vector itself.")
         print(I)
         print(D)
 
-    return gpu_index
-
-
-def search_faiss(index, query, k=5):
-    D, I = index.search(query, k)  # actual search
-    print(I[:5])  # neighbors of the 5 first queries
-    print(I[-5:])  # neighbors of the 5 last queries
+    return index
 
 
 if __name__ == '__main__':
@@ -86,4 +78,3 @@ if __name__ == '__main__':
             print(pred_label)
             print(metrics)
             print('--'*10)
-
