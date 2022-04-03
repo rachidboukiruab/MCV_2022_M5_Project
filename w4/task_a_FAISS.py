bee66f2ac1123c93b534b9bf87f8c392f5bb01aa
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 10:19:10 +0200

    debug prints

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     for row in range(plot_samples + 1):
         axs[row, 0].imshow(test_data[row][0].permute((1, 2, 0)).numpy())  # plots query img
         for column in range(1, k):
-            axs[row, column].imshow(find_in_train[pred_labels_list[row][0][column]][0])
+            axs[row, column].imshow(find_in_train[pred_labels_list[row][0][column]][0].permute((1, 2, 0)).numpy())
             print(f"for img {row}, nn id: {pred_labels_list[row][0][column]}")
 
     plt.title(f'{k} nearest imgs for firts {plot_samples}-th images (FAISS)')
