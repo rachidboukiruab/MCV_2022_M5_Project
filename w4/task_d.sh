b0234b08710572bfcd9cc47e364cab9c5c4f55a3
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 23:02:45 +0200

    3d

--- w4/task_d.sh
+++ w4/task_d.sh
@@ -5,4 +5,4 @@
 #SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
 #SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e %x_%u_%j.err # File to which STDERR will be written
-python task_d_TSNE.py
+python task_d_UMAP.py
