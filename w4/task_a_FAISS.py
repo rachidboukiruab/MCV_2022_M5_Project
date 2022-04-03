22a67e4e78fd62199778c1521d95098c046d8fca
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 09:12:45 +0200

    xb as float32

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -29,6 +29,7 @@ def build_index(model, train_dataset, d=32):
     for ii, (data, label) in enumerate(train_dataset):
         xb[ii, :] = model(data.unsqueeze(0)).squeeze().detach().numpy()
 
+    xb = np.float32(xb)
     print(xb.shape)
     index.add(xb)  # add vectors to the index
 
