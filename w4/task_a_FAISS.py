9381734c8a6c00c8e0044efa3ac62a704fb5c0f6
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 19:32:46 +0200

    MAp x class reports

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -10,64 +10,7 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import pickle
 from models import create_headless_resnet18
-
-
-def apk(actual, predicted, k=10):
-    """
-    Computes the average precision at k.
-    This function computes the average prescision at k between two lists of
-    items.
-    Parameters
-    ----------
-    actual : list
-             A list of elements that are to be predicted (order doesn't matter)
-    predicted : list
-                A list of predicted elements (order does matter)
-    k : int, optional
-        The maximum number of predicted elements
-    Returns
-    -------
-    score : double
-            The average precision at k over the input lists
-    """
-    if len(predicted) > k:
-        predicted = predicted[:k]
-
-    score = 0.0
-    num_hits = 0.0
-
-    for i, p in enumerate(predicted):
-        if p in actual and p not in predicted[:i]:
-            num_hits += 1.0
-            score += num_hits / (i + 1.0)
-
-    if not actual:
-        return 0.0
-
-    return score / min(len(actual), k)
-
-
-def mapk(actual, predicted, k=10):
-    """
-    Computes the mean average precision at k.
-    This function computes the mean average prescision at k between two lists
-    of lists of items.
-    Parameters
-    ----------
-    actual : list
-             A list of lists of elements that are to be predicted
-             (order doesn't matter in the lists)
-    predicted : list
-                A list of lists of predicted elements
-                (order matters in the lists)
-    k : int, optional
-        The maximum number of predicted elements
-    Returns
-    -------
-    score : double
-            The mean average precision at k over the input lists
-    """
-    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+from utils import mpk, mAP
 
 
 def build_index(model, train_dataset, d=32):
@@ -169,10 +112,13 @@ if __name__ == '__main__':
         gt_label_list_copy.append([zz])
     gt_label_list = gt_label_list_copy
 
-    scores_k1 = mapk(gt_label_list, pd_single, k=1)
-    scores_k5 = mapk(gt_label_list, pd_single, k=5)
-    print("MAP@1: ", scores_k1)
-    print("MAP@5: ", scores_k5)
+    p_1 = mpk(gt_label_list, pd_single, 1)
+    p_5 = mpk(gt_label_list, pd_single, 5)
+    print('P@1=', p_1)
+    print('P@5=', p_5)
+
+    map = mAP(gt_label_list, pd_single)
+    print('mAP=', map)
 
     # # For each class
     # # prepare data
