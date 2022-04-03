481ce0a75320ae10a230d2a2971cc57cf1389b64
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 11:15:47 +0200

    file name

--- w4/task_d_UMAP.py
+++ w4/task_d_UMAP.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     feature_path = Path("./results/retrieval")
     trained_path = Path("./results/jupytest")
 
-    weights_filename = "weights_1.pth"
+    weights_filename = "model.weights"
 
     EMBED_SHAPE = 64
 
