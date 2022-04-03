6d42f66bd1bcd52039a33291eb2e760c5d06ce62
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 22:12:14 +0200

    task d loads new weights

--- w4/task_d_UMAP.py
+++ w4/task_d_UMAP.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     feature_path = Path("./results/retrieval")
     trained_path = Path("./results/jupytest")
 
-    weights_filename = "model.weights"
+    weights_filename = "CONTRASTIVE.pth"
 
     EMBED_SHAPE = 32
 
