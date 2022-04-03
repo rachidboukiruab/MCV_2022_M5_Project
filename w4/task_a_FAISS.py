fb7828b0a7169285adae864f0501500ed69e04c6
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 18:30:44 +0200

    debug

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
 
     model = create_headless_resnet18(EMBED_SHAPE)
     model = model[:9]
-    index, find_in_train = build_index(model, test_data, d=512)
+    index, find_in_train = build_index(model, train_data, d=512)
 
     k = 4  # we want to see 10 nearest neighbors + the img itself
     query_data = np.empty((len(test_data), 512))
