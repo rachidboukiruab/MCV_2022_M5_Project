51d1d6dfead14baaeaaa8da1e91b9ff186edcdc4
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 22:17:23 +0200

    taskd sh

--- w4/task_d.sh
+++ w4/task_d.sh
@@ -3,6 +3,6 @@
 #SBATCH --mem 4096 # 4GB solicitados.
 #SBATCH -p mlow # or mlow Partition to submit to master low prioriy queue
 #SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
-#SBATCH -o ./slurm_logs/.%x_%u_%j.out # File to which STDOUT will be written
-#SBATCH -e ./slurm_logs/.%x_%u_%j.err # File to which STDERR will be written
+#SBATCH -o .%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e .%x_%u_%j.err # File to which STDERR will be written
 python task_d_UMAP.py
