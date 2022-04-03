a8b0a2a6c98f941155a73a46bafe1441322155b5
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 21:16:26 +0200

    label update 4 slides

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     metrics_list = list()
     with torch.no_grad():
         for ii, (img, label) in enumerate(test_data):
-            if label == 7:
+            if label == 0:
                 xq = model(img.unsqueeze(0)).squeeze().numpy()
                 xq = np.float32(xq)
                 metrics, pred_label = index.search(np.array([xq]), k)
