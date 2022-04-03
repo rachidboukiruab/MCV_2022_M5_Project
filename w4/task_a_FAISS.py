43bc334f583361c6669ab808aa6112f60a48d4aa
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-03 11:33:28 +0200

    file name

--- w4/task_a_FAISS.py
+++ w4/task_a_FAISS.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from ml_metrics import mapk
 import faiss
 import numpy as np
 import torch
@@ -13,6 +12,64 @@ from torchvision.datasets import ImageFolder
 from models import create_headless_resnet18
 from utils import print_colored, COLOR_WARNING
 from sklearn.metrics import average_precision_score
+
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
 
 
 def build_net(device, d=64):
