f9eece0f2457be01a060b43798d2d1e478bd3126
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-17 19:15:31 +0200

    debug

--- w5/m4rk0s/main.py
+++ w5/m4rk0s/main.py
@@ -40,6 +40,7 @@ for epoch in range(num_epochs):
         img_features, pos_text_features, neg_text_features = img_triple
         img_features, pos_text_features, neg_text_features = img_features.to(
             device), pos_text_features.to(device), neg_text_features.to(device)
+        print(img_features)
         image_encoded = image_model(img_features)
         pos_text_encoded = text_model(pos_text_features)
         neg_text_encoded = text_model(neg_text_features)
