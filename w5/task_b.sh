b60bd435aa550f699c2b1216ae6fb8dbd04b56eb
rachidboukir <rachid.boukir@autonoma.cat>
2022-04-18 14:03:10 +0200

    update

--- w5/task_b.sh
+++ w5/task_b.sh
@@ -5,4 +5,4 @@
 #SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
 #SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e %x_%u_%j.err # File to which STDERR will be written
-python task_a.py 12 1e-2 5e-4 32 0.7 2
+python task_b.py 12 1e-2 5e-4 32 0.7 2
