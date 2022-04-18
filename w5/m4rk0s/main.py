a95768ea6aea5e072e67a11b10008f1b7e1d8eef
imcosmicowl <marcos.melgar.segovia@gmail.com>
2022-04-18 10:19:08 +0200

    hyper-parameters with argsparser

--- w5/m4rk0s/main.py
+++ w5/m4rk0s/main.py
@@ -3,63 +3,82 @@ from torch import nn
 from torch.nn.utils.clip_grad import clip_grad_norm_
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from snd_models import ImgEncoder, TextEncoder
 from snd_utils import Img2TextDataset, decay_learning_rate
 
 img_features_file = '/home/group01/mcv/datasets/Flickr30k/vgg_feats.mat'
 text_features_file = '/home/group01/mcv/datasets/Flickr30k/fasttext_feats.npy'
-num_epochs = 12
-margin = 0.7
 
-loss_func = nn.TripletMarginLoss(margin, p=2)
+parser = ArgumentParser(
+        description='Torch-based image classification system',
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+parser.add_argument("num_epochs",
+                    type=int,
+                    help="Number of epochs")
+parser.add_argument("lr",
+                    type=float,
+                    help="learning rate")
+parser.add_argument("weight_decay",
+                    type=float,
+                    help="weight decay")
+parser.add_argument("batch_size",
+                    type=int,
+                    help="batch size")
 
-train_set = Img2TextDataset(img_features_file, text_features_file)
-train_dataloader = DataLoader(train_set, batch_size=32, shuffle=True, num_workers=4)
-grad_clip = 2
+args = parser.parse_args()
 
-# TEXT & IMGS MODELS
-image_model = ImgEncoder()
-text_model = TextEncoder()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-image_model.to(device)
-text_model.to(device)
+if __name__ == '__main__':
 
-# optimizer
-params = list(image_model.parameters())
-params += list(text_model.parameters())
+    loss_func = nn.TripletMarginLoss(0.7, p=2)
 
-optimizer = Adam(params, lr=1e-2, weight_decay=5e-4)
+    train_set = Img2TextDataset(img_features_file, text_features_file)
+    train_dataloader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    grad_clip = 2
 
-# training loop
-for epoch in range(num_epochs):
-    decay_learning_rate(1e-2, optimizer, epoch)
+    # TEXT & IMGS MODELS
+    image_model = ImgEncoder()
+    text_model = TextEncoder()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    image_model.to(device)
+    text_model.to(device)
 
-    for i, (img_triple, caption_triple) in enumerate(train_dataloader):
+    # optimizer
+    params = list(image_model.parameters())
+    params += list(text_model.parameters())
 
-        # execute image_triple
-        img_features, pos_text_features, neg_text_features = img_triple
-        img_features, pos_text_features, neg_text_features = img_features.to(
-            device), pos_text_features.to(device), neg_text_features.to(device)
-        image_encoded = image_model(img_features)
-        pos_text_encoded = text_model(pos_text_features)
-        neg_text_encoded = text_model(neg_text_features)
+    optimizer = Adam(params, lr=args.lr, weight_decay=args.weight_decay)
 
-        image_triple_loss = loss_func(image_encoded, pos_text_encoded, neg_text_encoded)
+    # training loop
+    for epoch in range(args.num_epochs):
+        decay_learning_rate(args.lr, optimizer, epoch)
 
-        # execute caption_triple
-        caption, pos_img, neg_img = caption_triple
-        caption, pos_img, neg_img = caption.to(device), pos_img.to(device), neg_img.to(device)
-        caption_encoded = text_model(caption)
-        pos_img_encoded = image_model(pos_img)
-        neg_img_encoded = image_model(neg_img)
-        caption_triple_loss = loss_func(caption_encoded, pos_img_encoded, neg_img_encoded)
+        for i, (img_triple, caption_triple) in enumerate(train_dataloader):
 
-        loss = image_triple_loss + caption_triple_loss
-        optimizer.zero_grad()
-        loss.backward()
-        if grad_clip > 0:
-            clip_grad_norm_(params, grad_clip)
-        optimizer.step()
+            # execute image_triple
+            img_features, pos_text_features, neg_text_features = img_triple
+            img_features, pos_text_features, neg_text_features = img_features.to(
+                device), pos_text_features.to(device), neg_text_features.to(device)
+            image_encoded = image_model(img_features)
+            pos_text_encoded = text_model(pos_text_features)
+            neg_text_encoded = text_model(neg_text_features)
 
-        print(f'epoch: {epoch}\titeration: {i}\tLoss: {loss}')
+            image_triple_loss = loss_func(image_encoded, pos_text_encoded, neg_text_encoded)
+
+            # execute caption_triple
+            caption, pos_img, neg_img = caption_triple
+            caption, pos_img, neg_img = caption.to(device), pos_img.to(device), neg_img.to(device)
+            caption_encoded = text_model(caption)
+            pos_img_encoded = image_model(pos_img)
+            neg_img_encoded = image_model(neg_img)
+            caption_triple_loss = loss_func(caption_encoded, pos_img_encoded, neg_img_encoded)
+
+            loss = image_triple_loss + caption_triple_loss
+            optimizer.zero_grad()
+            loss.backward()
+            if grad_clip > 0:
+                clip_grad_norm_(params, grad_clip)
+            optimizer.step()
+
+            print(f'epoch: {epoch}\titeration: {i}\tLoss: {loss}')
