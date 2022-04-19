f4b40b7ffeac1d82c5cbbe42d07c820df95e725f
rachidboukir <rachid.boukir@autonoma.cat>
2022-04-19 23:17:47 +0200

    Corrected some errors and added the model to gpu before evaluation

--- w5/task_d.py
+++ w5/task_d.py
@@ -3,6 +3,7 @@ import os
 import json
 import numpy as np
 from transformers import BertTokenizer, BertModel
+from time import time
 
 data_path = '/home/group01/mcv/datasets/Flickr30k'
 img_features_file = '{}/vgg_feats.mat'.format(data_path)
@@ -27,27 +28,33 @@ if __name__ == '__main__':
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    #bert_model = bert_model.to(device)
+    bert_model = bert_model.to(device)
 
+    print('Start extracting Text features from Train dataset...')
     #TRAIN DATA
     train_TextFeatures = []
     with torch.no_grad():
         bert_model.eval()
-
+        start_global_time = time()
         for i, key in enumerate(train_data):
             sentences = []
+            start = time()
             for j, sentence in enumerate(key['sentences']):
                 #print("{}, {}".format(i,sentence['raw']))
                 x = sentence['raw']
                 x = bert_tokenizer(x, return_tensors="pt")
+                x = x.to(device)
                 x = bert_model(**x)['last_hidden_state']
                 #print(x)
+                x = x.to("cpu")
                 sentences.append(np.array(x.squeeze().numpy()))
-            train_TextFeatures.append(np.array(sentences))
-            '''if i == 10:
+            train_TextFeatures.append(sentences)
+            end = time() - start
+            print('Time spent in image id {}: {:.3f} seconds'.format(i,end))
+            '''if i == 100:
                 break'''
 
-    train_TextFeatures = np.array(train_TextFeatures)
+    #train_TextFeatures = np.array(train_TextFeatures)
 
     #Save data
     os.makedirs(output_path, exist_ok=True)
@@ -55,27 +62,36 @@ if __name__ == '__main__':
         np.save(f, train_TextFeatures)       
     #print(train_TextFeatures)
     #print('shape: {}'.format(train_TextFeatures.shape))
+    end_global_time = time() - start_global_time
+    print('The evaluation has taken {:.3f} seconds.'.format(end_global_time))
     print('Text features from Train dataset saved.')
 
+    
+    print('Start extracting Text features from Validation dataset...')
     # VALIDATION DATA
     val_TextFeatures = []
     with torch.no_grad():
         bert_model.eval()
-
+        start_global_time = time()
         for i, key in enumerate(val_data):
             sentences = []
+            start = time()
             for j, sentence in enumerate(key['sentences']):
                 #print("{}, {}".format(i,sentence['raw']))
                 x = sentence['raw']
                 x = bert_tokenizer(x, return_tensors="pt")
+                x = x.to(device)
                 x = bert_model(**x)['last_hidden_state']
                 #print(x)
+                x = x.to("cpu")
                 sentences.append(np.array(x.squeeze().numpy()))
-            val_TextFeatures.append(np.array(sentences))
+            val_TextFeatures.append(sentences)
+            #end = time() - start
+            #print('Time spent in image id {}: {:.3f} seconds'.format(i,end))
             '''if i == 10:
                 break'''
 
-    val_TextFeatures = np.array(val_TextFeatures)
+    #val_TextFeatures = np.array(val_TextFeatures)
 
     #Save data
     os.makedirs(output_path, exist_ok=True)
@@ -83,27 +99,35 @@ if __name__ == '__main__':
         np.save(f, val_TextFeatures)       
     #print(train_TextFeatures)
     #print('shape: {}'.format(train_TextFeatures.shape))
+    end_global_time = time() - start_global_time
+    print('The evaluation has taken {:.3f} seconds.'.format(end_global_time))
     print('Text features from Validation dataset saved.')
 
+    print('Start extracting Text features from Test dataset...')
     # TEST DATA
     test_TextFeatures = []
     with torch.no_grad():
         bert_model.eval()
-
+        start_global_time = time()
         for i, key in enumerate(test_data):
             sentences = []
+            start = time()
             for j, sentence in enumerate(key['sentences']):
                 #print("{}, {}".format(i,sentence['raw']))
                 x = sentence['raw']
                 x = bert_tokenizer(x, return_tensors="pt")
+                x = x.to(device)
                 x = bert_model(**x)['last_hidden_state']
                 #print(x)
+                x = x.to("cpu")
                 sentences.append(np.array(x.squeeze().numpy()))
-            test_TextFeatures.append(np.array(sentences))
+            test_TextFeatures.append(sentences)
+            #end = time() - start
+            #print('Time spent in image id {}: {:.3f} seconds'.format(i,end))
             '''if i == 10:
                 break'''
 
-    test_TextFeatures = np.array(test_TextFeatures)
+    #test_TextFeatures = np.array(test_TextFeatures)
 
     #Save data
     os.makedirs(output_path, exist_ok=True)
@@ -111,4 +135,7 @@ if __name__ == '__main__':
         np.save(f, test_TextFeatures)       
     #print(train_TextFeatures)
     #print('shape: {}'.format(train_TextFeatures.shape))
+    end_global_time = time() - start_global_time
+    print('The evaluation has taken {:.3f} seconds.'.format(end_global_time))
     print('Text features from Test dataset saved.')
+    
