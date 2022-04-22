36e5dedb0cc25a88621684e5d0c13c2910eed8b7
rachidboukir <rachid.boukir@autonoma.cat>
2022-04-22 02:36:51 +0200

    Update task_d

--- w5/task_d_FeatureExtraction.py
+++ w5/task_d_FeatureExtraction.py
@@ -1,28 +1,49 @@
 import torch
+import wandb
+import numpy as np
+import umap
+import matplotlib.pyplot as plt
+
+from torch import nn
+from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.utils.data import DataLoader
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from models import ImgEncoder, TextEncoder, LinearEncoder
+from utils import decay_learning_rate, mpk
+from dataset import FlickrImagesAndCaptions
+
+from pytorch_metric_learning import miners, losses, reducers
+
+from sklearn.neighbors import KNeighborsClassifier
+
 import os
 import json
 import numpy as np
 from transformers import BertTokenizer, BertModel
 from time import time
 
+load_features = False
 data_path = '/home/group01/mcv/datasets/Flickr30k'
 output_path = "./results/task_d"
 os.makedirs(output_path, exist_ok=True)
 
-def extract_features(data):
+
+def extract_features(data, tokenizer, model, device):
     TextFeatures = []
     with torch.no_grad():
-        bert_model.eval()
+        model.eval()
         start_global_time = time()
         for i, key in enumerate(data):
             sentences = []
             start = time()
-            for j, sentence in enumerate(key['sentences']):
+            for sentence in key['sentences']:
                 #print("{}, {}".format(i,sentence['raw']))
                 x = sentence['raw']
-                x = bert_tokenizer(x, return_tensors="pt")
+                x = tokenizer(x, return_tensors="pt")
                 x = x.to(device)
-                x = bert_model(**x)['last_hidden_state']
+                x = model(**x)['last_hidden_state']
                 #print(x)
                 x = x.to("cpu")
                 sentences.append(np.array(x.squeeze().numpy()))
@@ -37,16 +58,18 @@ def extract_features(data):
     #train_TextFeatures = np.array(train_TextFeatures)
     return TextFeatures
 
-if __name__ == '__main__':
-
-    with open(f'{data_path}/train.json') as f:
+def BERT():
+    with open(f'{data_path}/dataset.json') as f:
+        dataset = json.load(f)
+    
+    '''with open(f'{data_path}/train.json') as f:
         train_data = json.load(f)
 
     with open(f'{data_path}/val.json') as f:
         val_data = json.load(f)
 
     with open(f'{data_path}/test.json') as f:
-        test_data = json.load(f)
+        test_data = json.load(f)'''
 
     bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     # Initializing a model from the bert-base-uncased style configuration
@@ -56,9 +79,18 @@ if __name__ == '__main__':
 
     bert_model = bert_model.to(device)
 
-    #TRAIN DATA
+    #ALL DATA
+    print('Start extracting Text features from Dataset...')
+    TextFeatures = extract_features(dataset['images'], bert_tokenizer, bert_model, device)
+
+    #Save data
+    with open('{}/bert_feats.npy'.format(output_path), "wb") as f:
+        np.save(f, TextFeatures)       
+    print('Text features saved.')
+
+    '''#TRAIN DATA
     print('Start extracting Text features from Train dataset...')
-    train_TextFeatures = extract_features(train_data)
+    train_TextFeatures = extract_features(train_data, bert_tokenizer, bert_model, device)
 
     #Save data
     with open('{}/train_Textfeatures.npy'.format(output_path), "wb") as f:
@@ -68,7 +100,7 @@ if __name__ == '__main__':
 
     # VALIDATION DATA  
     print('Start extracting Text features from Validation dataset...')
-    val_TextFeatures = extract_features(val_data)
+    val_TextFeatures = extract_features(val_data, bert_tokenizer, bert_model, device)
 
     #Save data
     with open('{}/val_Textfeatures.npy'.format(output_path), "wb") as f:
@@ -78,10 +110,28 @@ if __name__ == '__main__':
 
     print('Start extracting Text features from Test dataset...')
     # TEST DATA
-    test_TextFeatures = extract_features(test_data)
+    test_TextFeatures = extract_features(test_data, bert_tokenizer, bert_model, device)
 
     #Save data
     with open('{}/test_Textfeatures.npy'.format(output_path), "wb") as f:
         np.save(f, test_TextFeatures)       
     print('Text features from Test dataset saved.')
+
+    return train_TextFeatures, val_TextFeatures, test_TextFeatures'''
+    return TextFeatures
+
+
+if __name__ == '__main__':
+    if load_features:
+        #train_TextFeatures = np.load('{}/train_Textfeatures.npy'.format(output_path), allow_pickle=True)
+        #val_TextFeatures = np.load('{}/val_Textfeatures.npy'.format(output_path), allow_pickle=True)
+        #test_TextFeatures = np.load('{}/test_Textfeatures.npy'.format(output_path), allow_pickle=True)
+        TextFeatures = np.load('{}/bert_feats.npy'.format(output_path), allow_pickle=True)
+    else:
+        #train_TextFeatures, val_TextFeatures, test_TextFeatures = BERT()
+        TextFeatures = BERT()
+
+    
+
+    
     
