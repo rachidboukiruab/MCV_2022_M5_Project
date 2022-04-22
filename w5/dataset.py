819ed975e307fdbbcf30a7a1da0633662490bd04
rachidboukir <rachid.boukir@autonoma.cat>
2022-04-22 02:39:59 +0200

    Added FlickrImagesCaptionsBERT class

--- w5/dataset.py
+++ w5/dataset.py
@@ -189,3 +189,53 @@ class ImageData(Dataset):
         image = Image.open(os.path.join(self.img_path, self.images[idx]))
         tensor_img = self.tfms(image)
         return tensor_img
+
+class FlickrImagesAndCaptionsBERT(Dataset):
+    SPLITS = ["train", "val", "test"]
+
+    def __init__(
+            self,
+            dataset_path: str,
+            split: str
+    ):
+        root_path = Path(dataset_path)
+
+        assert ("./results/task_d/bert_feats.npy").exists(), "No textual features in data dir"
+        assert (root_path / "vgg_feats.mat").exists(), "No image features in data dir"
+        assert split in self.SPLITS, "Invalid dataset split"
+        assert (root_path / f"{split}.json").exists(), "No split data in data dir"
+
+        # To determine partition files, use imgid from partition jsons
+        with open(root_path / f"{split}.json", 'r') as f_json:
+            split = json.load(f_json)
+            indices = self._get_split_indices(split)
+
+        self.img_features = loadmat(str(root_path / "vgg_feats.mat"))['feats'].T[indices]
+        self.text_features = np.load("./results/task_d/bert_feats.npy", allow_pickle=True)
+        self.text_features = self._mean_reduction(self.text_features)[indices]
+
+    def __getitem__(self, index):
+        img_features = self.img_features[index]  # (Images, FeatureSize)
+        txt_features = self.text_features[index]  # (Images, FeatureSize)
+
+        return img_features, txt_features
+
+    def __len__(self):
+        return self.img_features.shape[0]
+
+    @staticmethod
+    def _get_split_indices(json_obj):
+        img_ids = []
+        for ii in json_obj:
+            img_ids.append(ii["imgid"])
+        return np.asarray(img_ids)
+
+    @staticmethod
+    def _mean_reduction(embeds):
+        aux1 = []
+        for i in range(len(embeds)):
+            aux2 = []
+            for sent in embeds[i]:
+                aux2.append(np.mean(sent, axis=0))
+            aux1.append(aux2)
+        return np.asarray(aux1)
